7d365273a46c66590af457cc9461261b59960147
sanpr <sanpr@SANTIAGO-HP>
2021-11-10 16:44:19 -0500

    hdskjad

--- LotsTaltuzaTechArchivosBinarios/src/co/edu/unbosque/view/PanelPartida.java
+++ LotsTaltuzaTechArchivosBinarios/src/co/edu/unbosque/view/PanelPartida.java
@@ -3,14 +3,13 @@ package co.edu.unbosque.view;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.GridLayout;
-
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
-//
+
 public class PanelPartida extends JPanel{
 	
 	private static final long serialVersionUID = 1L;
